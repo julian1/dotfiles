0516b9e3e540a9262fa8ee424c8c7bd707544ffc
julian1 <julian.atkinson71@gmail.com>
2016-01-31 17:40:27 +1100

    add volume script

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,12 +1,13 @@
 
+# could just use alias
+amixer set Master $1
+
+# amixer set Master 10%+
 # fixed 0-127
 # amixer set Master 50
 
 # percent
 # amixer set Master 50%
-
-amixer set Master 10%+
-
 
 # down
 # amixer set Master 10%-
