b888b2e3339f9e086250f133758e5fba384a620e
julian1 <mail@julian1.io>
2020-08-13 05:40:02 +1000

    tidy

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 # OK. there's an issue. that this replaces bashrc for nixos. but debian we want bashaliases
 
-for i in "bashrc" "vimrc" "screenrc"  "gitconfig" ; do 
-  echo $i; 
+for i in "bashrc" "vimrc" "screenrc"  "gitconfig" ; do
+  # echo $i;
   # ln -sf "$(pwd)/$i" "$HOME/.$i"
-  #rm  "$HOME/.$i"
+  # mv "$HOME/.$i" "$HOME/.$i.old"
 
-  cp "$(pwd)/$i" "$HOME/.$i"
+  cp -v "$(pwd)/$i" "$HOME/.$i"
 done
 
 # vim now hanles local dir creation,
-#for i in ".vim" ".vim/backup" ".vim/swap"  ".vim/undo" ".vim/autoload" ".vim/bundle"; do 
-#  echo $i; 
+#for i in ".vim" ".vim/backup" ".vim/swap"  ".vim/undo" ".vim/autoload" ".vim/bundle"; do
+#  echo $i;
 #  [ -d "$HOME/$i" ] ||  mkdir "$HOME/$i"
 #done
 
